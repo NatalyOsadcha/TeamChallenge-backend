7295ce741ee4d46c70918115acec8e1250d8690e
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-04-14 19:44:02 +0200

    register user function add

---
+++ controllers/usersControllers.js
@@ -0,0 +1,39 @@
+import * as userServices from "../services/usersServices.js";
+import bcrypt from "bcrypt";
+import HttpError from "../helpers/HttpError.js";
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
+import { registerUserSchema } from "../schemas/usersSchemas.js";
+import { nanoid } from "nanoid";
+
+
+export const registerUser = ctrlWrapper(async (req, res) => {
+    const { error } = registerUserSchema.validate(req.body);
+    if (error) {
+        res.status(400).json({ message: error.message });
+        return;
+    }
+    const { name, email, password } = req.body;
+
+
+    const existingUser = await userServices.findUser({ email });
+    if (existingUser) {
+        throw HttpError(409, "Email in use");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = nanoid();
+
+    const newUser = await userServices.signup({
+        name,
+        email,
+        password: hashedPassword,
+        verificationToken,
+    });
+
+    res.status(201).json({
+        user: {
+            name: newUser.name,
+            email: newUser.email,
+        }
+    });
+});
