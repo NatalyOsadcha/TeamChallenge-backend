e2bf837b63402271f1b7c26b3243fb2ec5f74de6
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-04-14 19:35:23 +0200

    usersRouter changed

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,13 @@
 import express from "express";
+import validateBody from "../helpers/validateBody.js";
+import { registerUserSchema } from "../schemas/usersSchemas.js";
+import { registerUser } from "../controllers/usersControllers.js"
 
 
 const usersRouter = express.Router();
 
 
-usersRouter.post("/register");
+usersRouter.post("/register", validateBody(registerUserSchema), registerUser);
 
 
 export default usersRouter;
